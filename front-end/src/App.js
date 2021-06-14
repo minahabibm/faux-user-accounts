8e1617a1b3ffd4d3886e0089a744d1c24695968c
Mina <mhcsc01@gmail.com>
2021-06-14 02:07:33 -0400

    Adding react project, adding dependencies, folder structure, and loading svg sprite

---
+++ front-end/src/App.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import WebFont from 'webfontloader';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
